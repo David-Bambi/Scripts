ca327c4ccca1a6f267de216951c7aa109010af40
David Boivin <davidboivinbambi@gmail.com>
2021-03-27 23:55:17 -0400

    Ajout des scripts existants.

---
+++ pull-db.ps1
@@ -0,0 +1,41 @@
+﻿# Répertoires
+$Backup = "D:\Backup"
+$DropboxDirectory = ".\Dropbox"
+$LocalDirectory = "D:\"
+$PulledDirectory = $args[0]
+
+# Créer un dossier backup pour y mettre un historique
+if (-NOT (Test-Path $Backup)) 
+{
+   New-Item -Path $Backup -ItemType Directory 
+}
+
+$operation = "_pull"
+$datetime = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
+
+# Vérifier si le dossier dans le dropbox existe
+if ((Test-Path "$DropboxDirectory\$PulledDirectory") -and
+    (Test-Path "$LocalDirectory\$PulledDirectory"))
+{
+    # Sauvegarder une nouvelle histoire
+    Move-Item -Path "$LocalDirectory\$PulledDirectory" -Destination "$Backup\$datetime$operation" -Force
+
+    $History = Get-ChildItem $Backup
+    # Si on a plus que 20 histoires on supprime les plus vielles histoires
+    $HistoryCount = ($History | Measure-Object ).Count
+    
+    if ($HistoryCount -gt 20)
+    {
+    
+        $SortedHistory = $History | Sort-Object CreationTime 
+        $HistoryToDelete = $SortedHistory[0]
+        Remove-Item -Path "$Backup\$HistoryToDelete" -Recurse -Force
+    }
+}
+
+if (Test-Path "$DropboxDirectory\$PulledDirectory")
+{
+    # TODO : Copier seulement les fichiers non présents ou modifier et supprimer les fichiers non présents
+    #        au lieu de tout supprimer.
+    Copy-Item -Path "$DropboxDirectory\$PulledDirectory" -Destination "$LocalDirectory\" -Recurse -Force
+}
---
+++ push-db.ps1
@@ -0,0 +1,41 @@
+﻿# Répertoires
+$Backup = "D:\Backup"
+$DropboxDirectory = ".\Dropbox"
+$LocalDirectory = "D:\"
+$PushedDirectory = $args[0]
+
+# Créer un dossier backup pour y mettre un historique
+if (-NOT (Test-Path $Backup)) 
+{
+   New-Item -Path $Backup -ItemType Directory 
+}
+
+$operation = "_push"
+$datetime = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
+
+# Vérifier si le dossier dans le dropbox existe pour l'historique
+if ((Test-Path "$DropboxDirectory\$PushedDirectory") -and
+    (Test-Path "$LocalDirectory\$PushedDirectory"))
+{
+    # Sauvegarder une nouvelle histoire
+    Move-Item -Path "$DropboxDirectory\$PushedDirectory" -Destination "$Backup\$datetime$operation" -Force
+
+    $History = Get-ChildItem $Backup
+    # Si on a plus que 20 histoires on supprime les plus vielles histoires
+    $HistoryCount = ($History | Measure-Object ).Count
+    
+    if ($HistoryCount -gt 20)
+    {
+    
+        $SortedHistory = $History | Sort-Object CreationTime 
+        $HistoryToDelete = $SortedHistory[0]
+        Remove-Item -Path "$Backup\$HistoryToDelete" -Recurse -Force
+    }
+}
+
+if (Test-Path "$LocalDirectory\$PushedDirectory")
+{
+    # TODO : Copier seulement les fichiers non présents ou modifier et supprimer les fichiers non présents
+    #        au lieu de tout supprimer.
+    Copy-Item -Path "$LocalDirectory\$PushedDirectory" -Destination "$DropboxDirectory\" -Recurse -Force
+}
